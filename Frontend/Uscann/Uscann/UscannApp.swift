14e180d0b01ab9adf728aa7e332e15439975dd01
AyazSheri <122620822+AyazSheri@users.noreply.github.com>
2024-11-17 19:02:58 -0600

    Update UscannApp.swift

--- Frontend/Uscann/Uscann/UscannApp.swift
+++ Frontend/Uscann/Uscann/UscannApp.swift
@@ -3,11 +3,21 @@
 //  Uscann
 //
 //
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct UscannApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             LoginView() // or whichever view you want to start with
