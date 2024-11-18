5949c45efb3deaa3386774e33a244d327ad2cd1e
Alex Vasiliev <alexv@Alexs-Mac-mini.local>
2024-11-17 19:19:38 -0600

    new frontend

---
+++ ChemicalInventory/ChemicalInventoryUITests/ChemicalInventoryUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ChemicalInventoryUITestsLaunchTests.swift
+//  ChemicalInventoryUITests
+//
+//  Created by Alex Vasiliev on 11/17/24.
+//
+
+import XCTest
+
+final class ChemicalInventoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
